5b8c4ec4450cc1d825367ad6509100e52ae98c0b
snpatel17 <savan.apjaks@gmail.com>
2021-02-21 15:29:30 -0500

    Custom Error Handling

---
+++ seeder.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+//Load env vars
+dotenv.config({ path: './config/config.env' });
+
+//Load models
+const Bootcamp = require('./models/Bootcamp');
+
+//Connect DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+//Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+//Impor into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data Imported'.green.inverse);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//Delete Data
+const deletData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data Destroyed'.red.inverse);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deletData();
+}
